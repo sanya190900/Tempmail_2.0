4269eaf7790576967d1db03480e2974f86e7b1a5
sanya190900 <48290799+sanya190900@users.noreply.github.com>
2020-05-09 11:48:30 -0400

    Add files via upload
    Version 09.05.2020

---
+++ index.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="TempMail.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Welcome to Tempmail</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+	<link rel="shotcut icon" type="image/x-icon" href="img/logo.png" />
+</head>
+<body>
+    <form id="form1" runat="server">
+		<div class="header">
+			<img src="img/logo.png" height="140px" style="margin-top: 10px;margin-left: 12%; float: left;" />
+			<h1 class="w3-text-white" style="margin-right: 18%; "><br />Welcome to Tempmail!</h1>
+		</div>
+        <div class="block">
+			<div class="text">
+				<p style="text-align: justify;">Nowadays an email address is a popular communication method, the practicality and other advantages of which are difficult to overestimate. Today many network resources, websites, programs allow you to activate your account or license solely by sending a control letter to an e-mail address. However, no one will ever guarantee that the email address entered anywhere will not be used by anyone to send unsolicited emails, which are called "spam". But how to be in this situation? The answer to this question is simple - your assistant will be a temporary mail, currently open to you.</p>
+
+				<p style="text-align: justify;">How to use this service: </p>
+				<ul style="text-align: justify;">
+					<li>To <b>get an individual e-mail address</b> you should click on the red button "Generate". Then service generates for you temporary e-mail address for 1 hour. If you exceed your time for your temporary e-mail address, it will be deleted. </li>
+					<li>To <b>extend your e-mail address</b> for an another hour you should click on the green button "Update" which located on the right side of the web-page. If you exceed your time for your temporary e-mail address and then click to this button - you will see start web-page without your individual e-mail address. </li>
+					<li>To <b>check your messages</b> you should click on the red button "Update Messages" which located on the right side of the web-page. This service can provide to you only 20 new messages. If you exceed the limit of received messages, you can see only 20 recent incoming messages. If you have not messages yet, you will see message "You have no messages".</li>
+					<li>To <b>clear the list of your messages</b> you should click on the blue button "Clear" which located on the right side of the web-page. Then you will see message "You have no messages".</li>
+					<li>To <b>delete your individual e-mail address</b> you should click on the yellow button "Delete Mail" which located on the right side of the web-page. Then your e-mail address and messages will be deleted.</li>
+				</ul>
+					<table border="0" width="100%">
+						<tr>
+							<td width="75%"><asp:TextBox ID="Mail" runat="server" CssClass="w3-input w3-border"></asp:TextBox></td>
+							<td style="padding-left: 20px;"><asp:Button CssClass="w3-btn w3-red w3-round-large" style="width: 200px;" ID="Generate" runat="server" Text="Generate" OnClick="Generate_Click" /></td>
+						</tr>
+					</table>
+				<h2>Your messages:</h2>
+                <p>
+					From: <asp:TextBox ID="From" runat="server"></asp:TextBox>
+					Subject: <asp:TextBox ID="Subject" runat="server"></asp:TextBox>
+					Date: <asp:TextBox ID="Date" runat="server"></asp:TextBox>
+					Text:<br /><asp:Label ID="Text" runat="server" Text="Label"></asp:Label>
+				</p>
+			</div>
+			<div class="btn">
+				<p style="margin-left: 10%;">
+					<asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+					<asp:UpdatePanel ID="UpdatePanel1" runat="server">
+						<ContentTemplate>
+							<asp:Timer ID="Timer1" runat="server" Interval="1000" ontick="Timer1_Tick"></asp:Timer>
+							<asp:Label ID="Timer" runat="server"></asp:Label>
+						</ContentTemplate>
+					</asp:UpdatePanel>
+                </p>
+				<table border="0" >
+					<tr>
+						<td><asp:Button CssClass="w3-btn w3-red w3-round-large" ID="updateMessages" style="width: 160px;" runat="server" Text="Update Messages" OnClick="updateMessages_Click" /></td>
+						<td><asp:Button CssClass="w3-btn w3-green w3-round-large" ID="updateMail" style="width: 80px; margin-left: 10px;" runat="server" Text="Update" OnClick="updateMail_Click" /></td>
+					</tr>
+				</table>
+				<table border="0">
+					<tr>
+						<td><button class="w3-btn w3-blue w3-round-large" name="clearMessages" style="width: 80px;">Clear</button></td>
+						<td><button class="w3-btn w3-yellow w3-round-large" name="deleteMail" style="width: 160px; margin-left: 10px;" >Delete Mail</button></td>
+					</tr>
+				</table>
+			</div>
+			<div class="footer"><p class="w3-text-white">Created by group IV-71<br />Copyright &copy; IV-71 (tempmail.com) 2019 All rights reserved</p></div>
+		</div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace TempMail
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (Session["CountdownTimer"] != null)
+                Timer.Text = "Expire email time: " + (Session["CountdownTimer"] as CountDownTimer).TimeLeft.ToString();
+        }
+        public class CountDownTimer
+        {
+            public TimeSpan TimeLeft;
+            System.Threading.Thread thread;
+            public CountDownTimer(TimeSpan original)
+            {
+                this.TimeLeft = original;
+            }
+            public void Start()
+            {
+                // Start a background thread to count down time
+                thread = new System.Threading.Thread(() =>
+                {
+                    while (true)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        TimeLeft = TimeLeft.Subtract(TimeSpan.Parse("00:00:01"));
+                        if (TimeLeft.ToString().Equals("00:00:00"))
+                            break;
+                    }
+                });
+                thread.Start();
+            }
+        }
+
+        protected void Generate_Click(object sender, EventArgs e)
+        {
+            if (Session["CountdownTimer"] == null)
+            {
+                Session["CountdownTimer"] = new CountDownTimer(TimeSpan.Parse("00:10:00"));
+                (Session["CountdownTimer"] as CountDownTimer).Start();
+                Timer.Text = "Expire email time: 00:10:00";
+            }
+        }
+
+        protected void updateMail_Click(object sender, EventArgs e)
+        {
+            String id = "1000.WOL2S5T3JJLPDITV6PZSADKFBJHPBR";
+            String secret = "be981947dd6d82e031110be2b5a4d2a283e2c6a7aa";
+            String refreshToken = "1000.ec59a11a13a38153d373b0d29c6ec22c.35ae7e90dd6246676e6cb02b4ad39a95";
+
+            Session["CountdownTimer"] = new CountDownTimer(TimeSpan.Parse("00:10:00"));
+            (Session["CountdownTimer"] as CountDownTimer).Start();
+            Timer.Text = "Expire email time: 00:10:00";
+
+            var request = (HttpWebRequest)WebRequest.Create("https://accounts.zoho.eu/oauth/v2/token");
+
+            var postData = "client_id=" + Uri.EscapeDataString(id);
+            postData += "&grant_type=" + Uri.EscapeDataString("refresh_token");
+            postData += "&client_secret=" + Uri.EscapeDataString(secret);
+            postData += "&refresh_token=" + Uri.EscapeDataString(refreshToken);
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            String responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            var details = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(responseString);
+
+            Session["access_token"] = details["access_token"];
+            Mail.Text = Session["access_token"].ToString();
+        }
+
+        protected void updateMessages_Click(object sender, EventArgs e)
+        {
+            if(Timer.Text.Equals("Expire email time: 00:00:00") || Timer.Text.Equals(""))
+            {
+                Session["access_token"] = null;
+                return;
+            }
+
+            var client = new RestClient("https://mail.zoho.eu/api/accounts/1305411000000002002/messages/view");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            String header = "Zoho-oauthtoken " + Session["access_token"].ToString();
+            request.AddHeader("Authorization", header);
+            IRestResponse response = client.Execute(request);
+            dynamic data = JObject.Parse(response.Content);
+            string from = data.data[0].sender;
+            string subject = data.data[0].subject;
+            string date = data.data[0].receivedTime;
+            string idMsg = data.data[0].messageId;
+
+            client = new RestClient("https://mail.zoho.eu/api/accounts/1305411000000002002/folders/1305411000000002014/messages/" + idMsg + "/content");
+            client.Timeout = -1; 
+            request = new RestRequest(Method.GET);
+            header = "Zoho-oauthtoken " + Session["access_token"].ToString();
+            request.AddHeader("Authorization", header);
+            response = client.Execute(request);
+
+            data = JObject.Parse(response.Content);
+            From.Text = from;
+            Subject.Text = subject;
+            Date.Text = date;
+            Text.Text = data["data"]["content"];
+        }
+    }
+}
